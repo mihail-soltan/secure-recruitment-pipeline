15325b63ec049a711d6d65a53815301f5a75479e
mihail <mihail.soltan@protonmail.ch>
2026-06-22 22:59:20 +0300

    add recruiter business logic

--- backend/app/services/job_service.py
+++ backend/app/services/job_service.py
@@ -41,3 +41,57 @@ async def process_job_application(conn: oracledb.Connection, job_id: int, candid
         raise e
     finally:
         cursor.close()
+
+
+def get_job_applications_for_recruiter(conn, job_id):
+    """returneaza aplicatiile procesate pentru un job"""
+    cursor = conn.cursor()
+    try:
+        cursor.execute("""
+            SELECT app_id, secure_resume_text, status, applied_at
+            FROM recruit_owner.JOB_APPLICATION
+            WHERE job_id = :1 AND status = 'PROCESSED_SECURE'
+        """, [job_id])
+        
+        columns = [col[0].lower() for col in cursor.description]
+        rows = cursor.fetchall()
+        applications = []
+        for row in rows:
+            app_dict = {}
+            for col_name, val in zip(columns, row):
+
+                if val is not None and hasattr(val, "read"):
+                    app_dict[col_name] = val.read()
+                else:
+                    app_dict[col_name] = val
+            applications.append(app_dict)
+            
+        return applications
+    finally:
+        cursor.close()
+
+def get_candidate_email_and_update_status(conn, app_id):
+    """extrage email-ul si schimba statusul in INTERVIEW"""
+    cursor = conn.cursor()
+    try:
+        # reidentificarea candidatului prin JOIN la nivel de DB
+        cursor.execute("""
+            SELECT c.email 
+            FROM recruit_owner.CANDIDATE c
+            JOIN recruit_owner.JOB_APPLICATION ja ON c.candidate_id = ja.candidate_id
+            WHERE ja.app_id = :1
+        """, [app_id])
+        row = cursor.fetchone()
+        email = row[0] if row else None
+        
+        if email:
+            cursor.execute("UPDATE recruit_owner.JOB_APPLICATION SET status='INTERVIEW' WHERE app_id=:1", [app_id])
+            conn.commit()
+            
+        return email
+    except Exception as e:
+        conn.rollback()
+        raise e
+    finally:
+        cursor.close()
+
